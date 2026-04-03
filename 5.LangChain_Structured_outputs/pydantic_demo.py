4d52e013d526a2b1f34a47de3ef4cf232135ce0d
Yes-Amrit <amugi2712@gmail.com>
2026-04-03 11:55:53 +0530

    Add CGPA field to Student model and implement structured output for reviews

--- 5.LangChain_Structured_outputs/pydantic_demo.py
+++ 5.LangChain_Structured_outputs/pydantic_demo.py
@@ -1,13 +1,17 @@
-from pydantic import BaseModel, EmailStr
+from pydantic import BaseModel, EmailStr, Field
 from typing import Optional
 
 class Student(BaseModel):
     name: str = 'amrit'
     age: Optional[int] = None
     email: str
+    CGPA: float = Field(gt=0, lt=10.5, default = 9)
 
-new_student = {'name':'raj', 'age':'21', 'email':'abc@gmail.com'}
+new_student = {'name':'raj', 'age':'21', 'email':'abc@gmail.com', 'CGPA':4}
 
 student = Student(**new_student)
 
-print(student)
+student_dict = student.model_dump()
+student_json = student.model_dump_json()
+print(student_dict['CGPA'])
+print(student_json)

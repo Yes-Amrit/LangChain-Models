d5d53c9923217ed4ae7a44cf661a802510841ba5
Yes-Amrit <amugi2712@gmail.com>
2026-05-14 22:51:59 +0530

    Add runnable sequence

---
+++ 8.Langchain-Runnables/runnable_sequence.py
@@ -0,0 +1,31 @@
+from dotenv import load_dotenv
+import os
+from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain_core.prompts import PromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.runnables import RunnableSequence
+
+load_dotenv()
+
+google_api_key = os.getenv("GOOGLE_API_KEY")
+
+llm = ChatGoogleGenerativeAI(
+    model="gemini-2.5-flash-lite",
+    google_api_key=google_api_key
+)
+
+prompt1 = PromptTemplate(
+    template='Write a joke about {topic}',
+    input_variables=['topic']
+)
+
+prompt2 = PromptTemplate(
+    template='Explain the following joke -{text}',
+    input_variables=['text']
+)
+
+parser = StrOutputParser()
+
+chain = RunnableSequence(prompt1, llm, parser, prompt2, llm, parser)
+
+print(chain.invoke({'topic':'AI'}))

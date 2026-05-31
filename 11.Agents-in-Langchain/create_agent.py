e317aaffa73f90607a85ed64bc8c438a7b0d9937
Yes-Amrit <amugi2712@gmail.com>
2026-05-31 20:31:50 +0530

    Agents in Langchain

---
+++ 11.Agents-in-Langchain/create_agent.py
@@ -0,0 +1,21 @@
+import os
+os.environ["GOOGLE_API_KEY"] = "AIzaSyCzu9nyTUOrinvkAr5VIRIIsYWUPNFjh5c"
+
+from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain_community.tools import DuckDuckGoSearchRun
+from langchain.agents import create_agent 
+
+
+search_tool = DuckDuckGoSearchRun()
+tools = [search_tool]
+llm = ChatGoogleGenerativeAI(model="gemini-2.5-flash")
+
+agent_executor = create_agent(
+    model=llm, 
+    tools=tools,
+    system_prompt="You are a helpful assistant. Use your search tool to find up-to-date facts."
+)
+query = "fastest stumping in cricket in the world"
+response = agent_executor.invoke({"messages": [("user", query)]})
+
+print(response["messages"][-1].content)
